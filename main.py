970d7e625ccabdd4846b18f8361d6b50ef14ba72
alexespino25 <107422380+alexespino25@users.noreply.github.com>
2022-08-18 14:49:59 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+datos = {"codigo": ["e-001", "e-002", "e-003", "e-004", "e-005"],
+         "nombre": ["pedro", "carlos", "juan", "Luis", "Alex"],
+         "cursos": ["Matematica", "quimica", "comunicación", "geografia", "historia"]}
+nota = []
+resp = "s"
+while resp == "s":
+    codigo = input("Ingrese el codigo del alumno: ")
+    curso = input("Ingresar el area: ")
+    nota1 = int(input("Ingresar la primera nota del alumno: "))
+    nota2 = int(input("Ingresar la segunda nota del alumno: "))
+    nota3 = int(input("Ingresar la tercera nota del alumno: "))
+    nota4 = int(input("Ingresar la cuarta nota del alumno: "))
+    x = 0
+    for i in datos ["codigo"]:
+        if i == codigo:
+            codigotemp = 1
+            nombretemp = datos ["nombre"][x]
+
+            promedio = (nota1 + nota2 + nota3 + nota4)/4
+            registro = ["codigo: " + str(codigotemp) + " | " + "nombre: " + str(nombretemp) + " | " + "curso: " + curso + " | " + "promedio: " + str(promedio) + "nota1" + str(nota1) + " | " + "nota2: " + str(nota2) + " | " + "nota3: " + str(nota3) + " | " + "nota4" + str(nota4)]
+            f = open("examen_final.txt", "a")
+            cadena = "".join(registro)
+            f.write("\n" +  cadena)
+            f.close()
+        x += 1
+    resp = input("¿Desea continuar ingresando datos? : s/n : ")
+    f = open("examen_final.txt")
+    print(f.read())
+    f.close()
+
